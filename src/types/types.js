2a42de78a7cc59fb79a262c7da0169c175ce74dc
Ignacio Arriagada <iarriagada@zionit.cl>
2022-02-21 14:10:38 -0300

    url variable de acceso a plataforam clientes

--- src/types/types.js
+++ src/types/types.js
@@ -1,9 +1,11 @@
+const TICKET_URL = process.env.REACT_APP_TICKETS_URL
+
 export const routes = {
-  home: '/',
-  login: '/login',
-  activity: '/actividades',
-  planner: '/planner',
-  times: '/informe-tiempos',
-  todo: '/to-do/:id',
-  plataforma_clientes: 'https://tickets.zproduccion.cl',
+   home: '/',
+   login: '/login',
+   activity: '/actividades',
+   planner: '/planner',
+   times: '/informe-tiempos',
+   todo: '/to-do/:id',
+   plataforma_clientes: TICKET_URL,
 }
