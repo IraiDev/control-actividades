fc60eb91fd2c767ff7b43363bf609be343f3e8b0
Ignacio Arriagada <iarriagada@zionit.cl>
2022-03-10 15:12:14 -0300

    solucion tiempo 0 a poner play

--- src/components/card/customCard/ActivityCard.jsx
+++ src/components/card/customCard/ActivityCard.jsx
@@ -96,7 +96,7 @@ const ActivityCard = props => {
    }
 
    const handleUpdateState = () => {
-      if (time === '' || time === '0' || time === 0) {
+      if (Number(time) <= 0) {
          Alert({
             icon: 'warn',
             title: 'Atencion',
