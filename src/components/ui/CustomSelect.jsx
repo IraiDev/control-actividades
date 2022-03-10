2b5c4473e857551045956d68cf1126292900e521
Ignacio Arriagada <iarriagada@zionit.cl>
2022-03-10 11:40:28 -0300

    solucion select mas tooltip, problema era webpack

--- src/components/ui/CustomSelect.jsx
+++ src/components/ui/CustomSelect.jsx
@@ -1,28 +1,28 @@
 import Select, { components } from 'react-select'
-// import Tooltip, {TooltipPrimitive} from '@atlaskit/tooltip'
-// import styled from '@emotion/styled';
+import Tooltip, {TooltipPrimitive} from '@atlaskit/tooltip'
+import styled from '@emotion/styled';
 
-// const InlineDialog = styled(TooltipPrimitive)`
-//   background: rgb(244, 244, 245);
-//   border-radius: 4px;
-//   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
-//   box-sizing: content-box; /* do not set this to border-box or it will break the overflow handling */
-//   color: #333;
-//   border: 1px solid #ccc;
-//   max-height: 300px;
-//   max-width: 300px;
-//   padding: 8px 12px;
-//   margin-left: 13px;
-// `
+const InlineDialog = styled(TooltipPrimitive)`
+  background: rgb(244, 244, 245);
+  border-radius: 4px;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+  box-sizing: content-box; /* do not set this to border-box or it will break the overflow handling */
+  color: #333;
+  border: 1px solid #ccc;
+  max-height: 300px;
+  max-width: 300px;
+  padding: 8px 12px;
+  margin-left: 13px;
+`
 
-// const Option = (props) => {
+const Option = (props) => {
 
-//    return (
-//       <Tooltip appearance='secundary' content={props?.data?.tooltip} position='right-end' component={InlineDialog}>
-//          <components.Option {...props} />
-//       </Tooltip>
-//    )
-// }
+   return (
+      <Tooltip appearance='secundary' content={props?.data?.tooltip} position='right-end' component={InlineDialog}>
+         <components.Option {...props} />
+      </Tooltip>
+   )
+}
 
 const CustomSelect = ({
    options,
